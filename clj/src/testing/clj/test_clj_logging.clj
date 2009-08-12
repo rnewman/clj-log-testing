00e348683e1d206a9b949103ef9d4404633dd9b9
Richard Newman <rnewman@twinql.com>
2009-08-11 22:32:25 -0700

    Reflect new clojure.contrib.logging changes; add code to turn on agent use.

--- clj/src/testing/clj/test_clj_logging.clj
+++ clj/src/testing/clj/test_clj_logging.clj
@@ -6,13 +6,24 @@
 (defn -main [& args]
   (binding [*out* *err*]
     (println "Starting Clojure test..."))
+  
+  ;; Forcibly engage agent logging.
+  (swap! *allow-direct-logging* (constantly false))
+  
   (let [start (long (System/currentTimeMillis))]
     (dotimes [x 10000]
       (log :info "Info.")
       (log :error "Error."))
-    (await *log-system-agent*)
+    
+    ;; For agent logging, wait for all log messages to be processed.
+    (when (not *allow-direct-logging*)
+      (await *logging-agent*))
+    
     (let [stop (long (System/currentTimeMillis))
           runtime (- stop start)]
       (binding [*out* *err*]
         (println "Run time for" (* 10000 2) "log messages:" runtime))))
-  (shutdown-agents))
+  
+  ;; Shutdown cleanly.
+  (when (not *allow-direct-logging*)
+    (shutdown-agents)))
